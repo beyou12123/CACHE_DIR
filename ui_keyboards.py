46e8933d639b47c3b7f42ab175f2fd769d2a2858
beyou12123 <abdullah.almoshkie255@gmail.com>
2026-05-02 22:44:21 +0300

    Add files via upload

--- ui_keyboards.py
+++ ui_keyboards.py
@@ -243,7 +243,11 @@ def get_keyboard(kays: int, m_status="OFF"):
 
 
 
+def get_coach_panel_keyboard(owner_id=None):
+    return get_keyboard(5)
 
+def get_tech_settings_keyboard(owner_id=None):
+    return get_keyboard(5)
 
 
    # main.py #المصنع
